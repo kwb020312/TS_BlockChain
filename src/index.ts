77f2aafb9e7fed899c5f9cdab5a60f64d4b61080
kwb020312 <kwb020312@naver.com>
2021-10-03 18:46:47 +0900

    #0.11 Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import * as CryptoJS from 'crypto-js';
 
 class Block {
+	static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+		CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+	static validateStructure = (aBlock: Block): boolean =>
+		typeof aBlock.index === 'number' &&
+		typeof aBlock.hash === 'string' &&
+		typeof aBlock.previousHash === 'string' &&
+		typeof aBlock.timestamp === 'string' &&
+		typeof aBlock.data === 'string';
+
 	public index: number;
 	public hash: string;
 	public previousHash: string;
 	public data: string;
 	public timestamp: number;
-
-	static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
-		CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
 	constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
 		this.index = index;
@@ -36,6 +43,16 @@ const createNewBlock = (data: string): Block => {
 	const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
 	const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
 	return newBlock;
+};
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+	if (!Block.validateStructure(candidateBlock)) {
+		return false;
+	} else if (previousBlock.index + 1 !== candidateBlock.index) {
+		return false;
+	} else if (previousBlock.hash !== candidateBlock.previousHash) {
+		return false;
+	}
 };
 
 export {};
