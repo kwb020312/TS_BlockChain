ea79025ae93886eafbdbdd5450efa42737186900
kwb020312 <kwb020312@naver.com>
2021-10-03 14:54:57 +0900

    #0.8 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-class Human {
-	public name: string;
-	public age: number;
-	public gender: string;
-	constructor(name: string, age: number, gender: string) {
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
+class Block {
+	public index: number;
+	public hash: string;
+	public previousHash: string;
+	public data: string;
+	public timestamp: number;
+	constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+		this.index = index;
+		this.hash = hash;
+		this.previousHash = previousHash;
+		this.data = data;
+		this.timestamp = timestamp;
 	}
 }
 
-const lynn = new Human('Lynn', 18, 'female');
+const genesisBlock: Block = new Block(0, '20202020202020', '', 'hello', 123456);
 
-const person = {
-	name: 'nicolas',
-	gender: 'male',
-	age: 22
-};
+let blockchain: [Block] = [ genesisBlock ];
 
-const sayHi = (person: Human): string => `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-
-sayHi(person);
+console.log(blockchain);
 
 export {};
